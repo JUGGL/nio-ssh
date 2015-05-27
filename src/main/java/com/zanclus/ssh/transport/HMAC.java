0922b7ded4ca1f085fa3e8cff97cb9de8b9e54f7
Deven Phillips <deven.phillips@gmail.com>
2015-05-27 10:24:25 -0400

    License headers

--- src/main/java/com/zanclus/ssh/transport/HMAC.java
+++ src/main/java/com/zanclus/ssh/transport/HMAC.java
@@ -1,9 +1,23 @@
+/**
+ * Copyright 2015, Deven Phillips
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zanclus.ssh.transport;
 
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.MD5Digest;
 import org.bouncycastle.crypto.digests.SHA1Digest;
-import org.bouncycastle.crypto.tls.MACAlgorithm;
 
 /**
  * Message Authentication Code ENUM type.

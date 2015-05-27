53bacd011996c258cad0eee82f8a39e62480a852
Deven Phillips <deven.phillips@gmail.com>
2015-05-27 14:05:13 -0400

    Stubbed initial unit tests for SSHFrameDecoder

---
+++ src/test/java/com/zanclus/ssh/transport/SSHFrameDecoderTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015, Deven Phillips
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zanclus.ssh.transport;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dphillips
+ */
+public class SSHFrameDecoderTest {
+    
+    public SSHFrameDecoderTest() {
+    }
+
+    /**
+     * Test of decode method, of class SSHFrameDecoder.
+     */
+    @Test
+    public void testDecode() throws Exception {
+    }
+
+    /**
+     * Test of largePacketSupport method, of class SSHFrameDecoder.
+     */
+    @Test
+    public void testLargePacketSupport_boolean() {
+    }
+
+    /**
+     * Test of largePacketSupport method, of class SSHFrameDecoder.
+     */
+    @Test
+    public void testLargePacketSupport_0args() {
+    }
+    
+}
